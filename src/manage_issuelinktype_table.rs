c4120ee95a9ade9cda372a81bea7227a89856323
Samuel DA MOTA <da.mota.sam@gmail.com>
2024-08-08 16:32:36 +0200

    IssueLinkType is now synchronised to the remote

--- src/manage_issuelinktype_table.rs
+++ src/manage_issuelinktype_table.rs
@@ -1,58 +1,67 @@
-use std::collections::HashSet;
-use sqlx::{FromRow, Pool, Sqlite};
 use crate::get_config::Config;
 use crate::get_json_from_url::get_json_from_url;
 use crate::get_str_for_key;
-use crate::utils::get_inputs_in_remote_not_in_db;
+use crate::utils::{get_inputs_in_db_not_in_remote, get_inputs_in_remote_not_in_db};
+use sqlx::{FromRow, Pool, Sqlite};
+use std::collections::HashSet;
 
 #[derive(FromRow, Debug, Eq, PartialEq, Hash)]
 pub(crate) struct IssueLinkType {
-  jira_id: u32,
-  name: String,
-  outward_name: String,
-  inward_name: String,
+    jira_id: u32,
+    name: String,
+    outward_name: String,
+    inward_name: String,
 }
 
 async fn get_link_types_from_database(db_conn: &Pool<Sqlite>) -> Vec<IssueLinkType> {
-  let query_str =
-    "SELECT  jira_id, name, outward_name, inward_name
+    let query_str = "SELECT  jira_id, name, outward_name, inward_name
          FROM IssueLinkType;";
 
-  let rows = sqlx::query_as::<_, IssueLinkType>(query_str)
-    .fetch_all(db_conn)
-    .await;
+    let rows = sqlx::query_as::<_, IssueLinkType>(query_str)
+        .fetch_all(db_conn)
+        .await;
 
-  match rows {
-    // todo(perf) simply rename fields in ProjectShortData to avoid the need of this conversion
-    Ok(data) => { data }
-    Err(e) => {
-      eprintln!("Error occurred while trying to get link types from local database: {e}");
-      Vec::new()
+    match rows {
+        Ok(data) => data,
+        Err(e) => {
+            eprintln!("Error occurred while trying to get link types from local database: {e}");
+            Vec::new()
+        }
     }
-  }
 }
 
+async fn get_issue_link_types_from_server(config: &Config) -> Result<Vec<IssueLinkType>, String> {
+    let query = "/rest/api/2/issueLinkType";
+    let json_data = get_json_from_url(config, query).await;
+    let Ok(json_data) = json_data else {
+        return Err(format!(
+            "Error: failed to get list of link types from server.\n{e}",
+            e = json_data.err().unwrap().to_string()
+        ));
+    };
 
-async fn get_issue_link_types_from_server(config: &Config) -> Result<Vec<IssueLinkType>, String>{
-  let query = "/rest/api/2/issueLinkType";
-  let json_data = get_json_from_url(config, query).await;
-  let Ok(json_data) = json_data else {
-    return Err(format!("Error: failed to get list of link types from server.\n{e}", e=json_data.err().unwrap().to_string()));
-  };
+    let Some(json_array) = json_data.as_object() else {
+        return Err(format!(
+            "Error: Returned data is unexpected. Expecting a json object, got [{e}]",
+            e = json_data.to_string()
+        ));
+    };
 
-  let Some(json_array) = json_data.as_object() else {
-    return Err(format!("Error: Returned data is unexpected. Expecting a json object, got [{e}]", e = json_data.to_string()));
-  };
+    let Some(json_array) = json_array.get("issueLinkTypes") else {
+        return Err(format!(
+            "Error: Returned data is unexpected. Expecting a key named issueLinkTypes, got [{e}]",
+            e = json_data.to_string()
+        ));
+    };
 
-  let Some(json_array) = json_array.get("issueLinkTypes") else {
-    return Err(format!("Error: Returned data is unexpected. Expecting a key named issueLinkTypes, got [{e}]", e = json_data.to_string()));
-  };
+    let Some(json_array) = json_array.as_array() else {
+        return Err(format!(
+            "Error: Returned data is unexpected. Expecting a json array, got [{e}]",
+            e = json_data.to_string()
+        ));
+    };
 
-  let Some(json_array) = json_array.as_array() else {
-    return Err(format!("Error: Returned data is unexpected. Expecting a json array, got [{e}]", e = json_data.to_string()));
-  };
-
-  let res = json_array
+    let res = json_array
     .iter()
     .filter_map(|x| {
       let name = get_str_for_key(&x, "name")?;
@@ -76,91 +85,137 @@ async fn get_issue_link_types_from_server(config: &Config) -> Result<Vec<IssueLi
     })
     .collect::<Vec<_>>();
 
-//    dbg!(&res);
-  Ok(res)
+    //    dbg!(&res);
+    Ok(res)
 }
 
-
-fn get_link_types_not_in_db<'a, 'b>(link_types: &'a Vec<IssueLinkType>, link_types_in_db: &'b Vec<IssueLinkType>)
-                                    -> Vec<&'a IssueLinkType>
-  where 'b: 'a
+fn get_link_types_in_remote_not_in_db<'a, 'b>(
+    link_types: &'a Vec<IssueLinkType>,
+    link_types_in_db: &'b Vec<IssueLinkType>,
+) -> Vec<&'a IssueLinkType>
+where
+    'b: 'a,
 {
-  get_inputs_in_remote_not_in_db(link_types.as_slice(), link_types_in_db.as_slice())
+    get_inputs_in_remote_not_in_db(link_types.as_slice(), link_types_in_db.as_slice())
 }
 
-async fn insert_issue_link_types_to_database(db_conn: &mut Pool<Sqlite>, issue_link_types_to_insert: Vec<&IssueLinkType>) {
-  if issue_link_types_to_insert.is_empty() {
-    eprintln!("No new link type found");
-    return;
-  }
+fn get_link_types_in_db_not_in_remote<'a>(
+    link_types_in_remote: &'a Vec<IssueLinkType>,
+    link_types_in_db: &'a Vec<IssueLinkType>,
+) -> Vec<&'a IssueLinkType> {
+    get_inputs_in_db_not_in_remote(link_types_in_remote.as_slice(), link_types_in_db.as_slice())
+}
 
-  let mut has_error = false;
-  let mut row_affected = 0;
-  let mut tx = db_conn
-    .begin()
-    .await
-    .expect("Error when starting a sql transaction");
+pub(crate) async fn update_issue_link_types_in_db(config: &Config, db_conn: &mut Pool<Sqlite>) {
+    let issue_link_types_in_remote = get_issue_link_types_from_server(&config).await;
+    let Ok(issue_link_types_in_remote) = issue_link_types_in_remote else {
+        eprintln!(
+            "Error: failed to get link types from server: Err=[{e}]",
+            e = issue_link_types_in_remote.err().unwrap()
+        );
+        return;
+    };
+    let issue_link_types_in_db = get_link_types_from_database(&db_conn).await;
+    let issue_link_types_to_insert =
+        get_link_types_in_remote_not_in_db(&issue_link_types_in_remote, &issue_link_types_in_db);
+    let issue_link_types_to_remove =
+        get_link_types_in_db_not_in_remote(&issue_link_types_in_remote, &issue_link_types_in_db);
 
-  // todo(perf): these insert are likely very inefficient since we insert
-  // one element at a time instead of doing bulk insert.
-  // check https://stackoverflow.com/questions/65789938/rusqlite-insert-multiple-rows
-  // and https://www.sqlite.org/c3ref/c_limit_attached.html#sqlitelimitvariablenumber
-  // for the SQLITE_LIMIT_VARIABLE_NUMBER maximum number of parameters that can be
-  // passed in a query.
-  // splitting an iterator in chunks would come in handy here.
+    match issue_link_types_to_remove.is_empty() {
+      true => {eprintln!("No issue link type found in local db that isn't also in the remote");}
+      false => {
+        let query_str = "DELETE FROM IssueLinkType
+                      WHERE jira_id = ?;";
 
-  // todo(perf): add detection of what is already in db and do some filter out. Here we happily
-  // overwrite data with the exact same ones, thus taking the write lock on the
-  // database for longer than necessary.
-  // Plus it means the logs aren't that useful to troubleshoot how much data changed
-  // in the database. Seeing messages saying
-  // 'updated Issue fields in database: 58 rows were updated'
-  // means there has been at most 58 changes. Chances are there are actually been
-  // none since the last update.
-  let query_str =
+        let mut has_error = false;
+        let mut row_affected = 0;
+        let mut tx = db_conn
+          .begin()
+          .await
+          .expect("Error when starting a sql transaction");
+
+        for IssueLinkType{ jira_id, name, outward_name, inward_name } in issue_link_types_to_remove
+        {
+          let res = sqlx::query(query_str)
+            .bind(jira_id)
+            .execute(&mut *tx)
+            .await;
+
+          match res {
+            Ok(e) => row_affected += e.rows_affected(),
+            Err(e) => {
+              has_error = true;
+              eprintln!("Error when removing an issue link type with jira_id {jira_id}, name: {name}, outward_name: {outward_name}, inward_name: {inward_name}: Err {e}");
+            }
+          }
+        }
+
+        tx.commit().await.unwrap();
+
+        if has_error {
+          eprintln!("Error occurred while removing issue link type from the local database")
+        } else {
+          eprintln!("updated issue link type in database: {row_affected} rows were deleted")
+        }
+      }
+    }
+
+    match issue_link_types_to_insert.is_empty() {
+        true => {
+            eprintln!("No new issue link type found");
+        }
+        false => {
+            let mut has_error = false;
+            let mut row_affected = 0;
+            let mut tx = db_conn
+                .begin()
+                .await
+                .expect("Error when starting a sql transaction");
+
+            // todo(perf): these insert are likely very inefficient since we insert
+            // one element at a time instead of doing bulk insert.
+            // check https://stackoverflow.com/questions/65789938/rusqlite-insert-multiple-rows
+            // and https://www.sqlite.org/c3ref/c_limit_attached.html#sqlitelimitvariablenumber
+            // for the SQLITE_LIMIT_VARIABLE_NUMBER maximum number of parameters that can be
+            // passed in a query.
+            // splitting an iterator in chunks would come in handy here.
+
+            let query_str =
     "INSERT INTO IssueLinkType (jira_id, name, outward_name, inward_name) VALUES
                 (?, ?, ?, ?)
             ON CONFLICT DO
             UPDATE SET name = excluded.name, inward_name = excluded.inward_name, outward_name = excluded.outward_name";
 
-  for IssueLinkType { jira_id, name, outward_name, inward_name } in issue_link_types_to_insert {
-    let res = sqlx::query(query_str)
-      .bind(jira_id)
-      .bind(name)
-      .bind(outward_name)
-      .bind(inward_name)
-      .execute(&mut *tx)
-      .await;
-    match res {
-      Ok(e) => { row_affected += e.rows_affected() }
-      Err(e) => {
-        has_error = true;
-        eprintln!("Error: {e}")
-      }
+            for IssueLinkType {
+                jira_id,
+                name,
+                outward_name,
+                inward_name,
+            } in issue_link_types_to_insert
+            {
+                let res = sqlx::query(query_str)
+                    .bind(jira_id)
+                    .bind(name)
+                    .bind(outward_name)
+                    .bind(inward_name)
+                    .execute(&mut *tx)
+                    .await;
+                match res {
+                    Ok(e) => row_affected += e.rows_affected(),
+                    Err(e) => {
+                        has_error = true;
+                        eprintln!("Error: {e}")
+                    }
+                }
+            }
+
+            tx.commit().await.unwrap();
+
+            if has_error {
+                eprintln!("Error occurred while updating the database with Link types")
+            } else {
+                eprintln!("updated Link types in database: {row_affected} rows were updated")
+            }
+        }
     }
-  }
-
-  tx.commit().await.unwrap();
-
-  if has_error {
-    eprintln!("Error occurred while updating the database with Link types")
-  } else {
-    eprintln!("updated Link types in database: {row_affected} rows were updated")
-  }
 }
-
-
-pub(crate)
-async fn update_issue_link_types_in_db(config: &Config, db_conn: &mut Pool<Sqlite>) {
-  let link_types_to_insert = get_issue_link_types_from_server(&config).await;
-  let Ok(link_types_to_insert) = link_types_to_insert else {
-    eprintln!("Error: failed to get link types from server: Err=[{e}]", e = link_types_to_insert.err().unwrap());
-    return;
-  };
-  let link_types_in_db = get_link_types_from_database(&db_conn).await;
-  let links_to_insert = get_link_types_not_in_db(&link_types_to_insert, &link_types_in_db);
-
-
-  insert_issue_link_types_to_database(db_conn, links_to_insert).await;
-}
-
